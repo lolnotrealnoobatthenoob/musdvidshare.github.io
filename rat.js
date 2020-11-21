7132b91a95b3bec67720a88130a3cdd8f1966c66
lolnotrealnoobatthenoob <70600851+lolnotrealnoobatthenoob@users.noreply.github.com>
2020-11-21 14:46:23 -0800

    Added New Files

---
+++ rat.js
@@ -0,0 +1,9 @@
+function addlike() {
+    alert('You Have Liked This Video. Thank you for your feedback!');
+}
+function adddislike() {
+    alert('You Have Disliked This Video. Thank you for your feedback!');
+}
+function addcom() {
+    alert('You Have Commented on This Video. Thank you for your feedback!')
+}
